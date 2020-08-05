6c5a790dd35163afe393114e123625062924e1ec
purirahul <rpuri648@gmail.com>
2020-08-05 21:43:50 +0530

    Added filter buttons for news

--- src/FirstNews.js
+++ src/FirstNews.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css"
 
 
 class FirstNews extends Component{
-
-    state ={
+  constructor(){
+    super();
+    this.state ={
       list: null,
-  };
+  }
+
+  this.getScience = this.getScience.bind(this);
+  this.getBusiness = this.getBusiness.bind(this);
+  this.getSports = this.getSports.bind(this);
+  this.getHealth = this.getHealth.bind(this);
+  this.getEntertainment = this.getEntertainment.bind(this);
+};
 
 
   componentDidMount(){
@@ -19,10 +26,49 @@ class FirstNews extends Component{
     });
   }
 
+
+  getScience(){
+    axios.get('https://gnews.io/api/v3/topics/science?country=in&token=0e5ea080bb48fc6a975fba5874967685').then(res =>{
+      this.setState({list: res.data.articles});
+    });
+  }
+
+
+  getBusiness(){
+    axios.get('https://gnews.io/api/v3/topics/business?country=in&token=0e5ea080bb48fc6a975fba5874967685').then(res =>{
+      this.setState({list: res.data.articles});
+    });
+  }
+
+  getSports(){
+    axios.get('https://gnews.io/api/v3/topics/sports?country=in&token=0e5ea080bb48fc6a975fba5874967685').then(res =>{
+      this.setState({list: res.data.articles});
+    });
+  }
+
+  getHealth(){
+    axios.get('https://gnews.io/api/v3/topics/health?country=in&token=0e5ea080bb48fc6a975fba5874967685').then(res =>{
+      this.setState({list: res.data.articles});
+    });
+  }
+
+  getEntertainment(){
+    axios.get('https://gnews.io/api/v3/topics/entertainment?country=in&token=0e5ea080bb48fc6a975fba5874967685').then(res =>{
+      this.setState({list: res.data.articles});
+    });
+  }
+
 render(){
   return(
-    <div>
-    <div className="NewsContainer" style={{textAlign:"left"}}>
+    <div className="container">
+      <div className="row mb-3 mt-4">
+        <button className="btn btn-primary" onClick={this.getScience} style={{margin:"5px"}}>Science</button>
+        <button className="btn btn-primary" onClick={this.getBusiness} style={{margin:"5px"}}>Business</button>
+        <button className="btn btn-primary" onClick={this.getSports} style={{margin:"5px"}}>Sports</button>
+        <button className="btn btn-primary" onClick={this.getHealth} style={{margin:"5px"}}>Health</button>
+        <button className="btn btn-primary" onClick={this.getEntertainment} style={{margin:"5px"}}>Entertainment</button>
+      </div>
+    <div  style={{textAlign:"left"}}>
     {this.state.list ? this.state.list.map((list, i) =>
 
       <div className="card bg-light border-light rounded shadow-lg mb-3 " key={i} style={{maxWidth: "100%"}} >
@@ -36,6 +82,7 @@ render(){
                                   <p className="card-text"><small className="text-muted">{list.source.name}</small></p>
                                   <p className="card-text"><a href={list.url}>{list.description}</a></p>
                                   <p className="card-text"><small className="text-muted">{list.publishedAt}</small></p>
+                                  <a href={list.url}><p className="card-text"><small className="text-muted">visit</small></p></a>
                           </div>
                     </div>
                 </div>
